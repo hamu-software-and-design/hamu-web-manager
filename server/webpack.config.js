13176ddecc90f1d63f36caa6a6cc9dcdf761f36c
vangogh500 <matsudaskai@gmail.com>
2017-06-23 21:28:11 +0700

    fixed backend compilation target

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: __dirname +'/build/',
-    filename: 'index.js'
+    path: __dirname + '/bin/',
+    publicPath: 'bin/',
+    filename: 'server.js'
   },
   target: 'node',
   module: {
