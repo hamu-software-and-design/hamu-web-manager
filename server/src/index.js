37b9b91d927bf4572a40308233b860243afdbf10
vangogh500 <matsudaskai@gmail.com>
2017-06-24 02:46:22 +0700

    figured out serverside env for es5

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,17 @@
 import Express from 'express'
+import path from 'path'
+import Mongoose from 'mongoose'
+
+import credentials from './credentials.js'
 
 const app = new Express()
+Mongoose.connect(credentials.mongodb.url)
+
+app.use(Express.static(path.resolve(__dirname + '/../../client/build')))
+
+app.get('*', function(req,res) {
+  res.sendFile(path.resolve(__dirname, '/../../client/build', 'index.html'))
+})
 
 app.listen(3000, function() {
   console.log('server is listening on port 3000')
